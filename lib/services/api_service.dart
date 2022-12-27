cee99535ebeaa6e2b94dd3e0606fcd0a2f6acc09
SabinSnider <sabinvsic@gmail.com>
2022-12-27 10:53:18 +0545

    Http call added in services

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,29 @@
 //HTTP request service, get http resqust form API and return list of Articels
 
-class ApiService {
+import 'dart:convert';
 
-  final endPointUrl =""
+import 'package:http/http.dart';
+
+import '../model/article_model.dart';
+
+class ApiService {
+  final endPointUrl =
+      "https://newsapi.org/v2/everything?q=Apple&from=2022-12-27&sortBy=popularity&apiKey=892977179ae544fe918b6964670357c1";
+
+//http request function
+
+  Future<List<Article>> getArticle() async {
+    Response res = await get(Uri.parse(endPointUrl));
+    //check 200 status
+    if (res.statusCode == 200) {
+      Map<String, dynamic> json = jsonDecode(res.body);
+      List<dynamic> body = json['articles'];
+      //To get different articles from json file and put the into a list
+      List<Article> articles =
+          body.map((dynamic item) => Article.fromJson(item)).toList();
+      return articles;
+    } else {
+      throw ("Can't get the Articles");
+    }
+  }
 }
